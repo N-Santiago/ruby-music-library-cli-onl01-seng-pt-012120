dda5f041b495a14d1cb32625ad707ccea42d9ed0
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-28 23:20:30 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -74,7 +74,6 @@ class MusicLibraryController
     if genre = Genre.find_by_name(input)
       genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
         puts "#{i+1}. #{s.artist.name} - #{s.name}"
-        binding.pry 
       end
     end
   end
